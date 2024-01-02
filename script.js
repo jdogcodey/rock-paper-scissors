ccbe2d1516257530f28f76b180ea8785a0838596
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-01-02 16:34:16 +0000

    Created Computer Random Output

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let options = ["Rock", "Paper", "Scissors"];
+    let random = Math.floor((Math.random() * options.length));
+    let randomChoice=options[random];
+    return randomChoice;
+}
+
+console.log(getComputerChoice())
