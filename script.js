af6fe46a5ae35f25cbe0e28606a15978b1f3c27a
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-02-08 22:46:01 +0000

    Complete game functionality
    Took forever to realise why it wasn't working exactly why I wanted.
    Really good learning about where to place things in the code to make
    sure that they operate as desired.
    
    Now to make it look nice

--- script.js
+++ script.js
@@ -1,54 +1,10 @@
 let computerScore = 0;
 let playerScore = 0;
-let runningHigh = Math.max(computerScore, playerScore)
+let runningHigh = 0;
 let playerSelectionLC = '';
 let i = 0;
-const scoreTracker = document.createElement('div');
-const results = document.createElement('div');
 
-function playRound() {
-    function getComputerChoice() {
-        let options = ["rock", "paper", "scissors"];
-        let random = Math.floor((Math.random() * options.length));
-        let randomChoice = options[random];
-        return randomChoice;
-    };
-
-    let computerChoice = getComputerChoice();
-
-    console.log('The Computer Chose ' + computerChoice);
-
-    let totalValue = playerSelectionLC + computerChoice;
-
-    function outcome() {
-        if (totalValue == 'rockscissors' || totalValue == 'paperrock' || totalValue == 'scissorspaper') {
-            playerScore++;
-            return 'You WIN because ' + playerSelectionLC + ' beats ' + computerChoice;
-        }
-        else if (totalValue == 'rockpaper' || totalValue == 'scissorsrock' || totalValue == 'paperscissors') {
-            ++computerScore;
-            return 'Unlucky... You lose because the Computer chose ' + computerChoice + ' which beats your ' + playerSelectionLC;
-        }
-        else if (totalValue == 'scissorsscissors' || totalValue == 'paperpaper' || totalValue == 'rockrock')  {
-            return "You think like a Computer - it's a TIE as you both chose " + computerChoice
-        }
-        else return 'Something went wrong... Did you spell your choice correctly?'
-    };
-    results.textContent = outcome();
-    document.body.appendChild(results);
-    function score() {
-        if (playerScore > computerScore) {
-           return `You're Winning! SCORE: Player-${playerScore} : ${computerScore}-Computer`;
-        }
-        else {
-            return `You're Losing SCORE: Player-${playerScore} : ${computerScore}-Computer`;
-        };
-    }
-    scoreTracker.textContent = score();
-    document.body.appendChild(scoreTracker);
-};
-
-
+    console.log(`${runningHigh} game`)
 const rock = document.createElement('button');
 rock.textContent = 'Rock';
 document.body.appendChild(rock);
@@ -72,4 +28,71 @@ scissors.addEventListener('click', () => {
     playerSelectionLC = 'scissors';
     playRound();
 });
+
+const scoreTracker = document.createElement('div');
+const results = document.createElement('div');
+
+
+function playRound() {
+    console.log(`${runningHigh} playRound`)
+        function getComputerChoice() {
+            let options = ["rock", "paper", "scissors"];
+            let random = Math.floor((Math.random() * options.length));
+            let randomChoice = options[random];
+            return randomChoice;
+        };
+
+        let computerChoice = getComputerChoice();
+
+        let totalValue = playerSelectionLC + computerChoice;
+
+        function outcome() {
+            if (totalValue == 'rockscissors' || totalValue == 'paperrock' || totalValue == 'scissorspaper') {
+                playerScore++;
+                return 'You WIN because ' + playerSelectionLC + ' beats ' + computerChoice;
+            }
+            else if (totalValue == 'rockpaper' || totalValue == 'scissorsrock' || totalValue == 'paperscissors') {
+                ++computerScore;
+                return 'Unlucky... You lose because the Computer chose ' + computerChoice + ' which beats your ' + playerSelectionLC;
+            }
+            else if (totalValue == 'scissorsscissors' || totalValue == 'paperpaper' || totalValue == 'rockrock')  {
+                return "You think like a Computer - it's a TIE as you both chose " + computerChoice
+            }
+            else return 'Something went wrong... Did you spell your choice correctly?'
+        };
+        results.textContent = outcome();
+        document.body.appendChild(results);
+        function score() {
+            if (playerScore > computerScore) {
+            return `You're Winning! SCORE: Player-${playerScore} : ${computerScore}-Computer`;
+            }
+            else {
+                return `You're Losing SCORE: Player-${playerScore} : ${computerScore}-Computer`;
+            };
+        }
+        scoreTracker.textContent = score();
+        document.body.appendChild(scoreTracker);
+        runningHigh = Math.max(computerScore, playerScore);
+        if (runningHigh >= 5) {
+            rock.remove();
+            paper.remove();
+            scissors.remove();
+            results.remove();
+            scoreTracker.remove();
+            function final() {
+                if (playerScore > computerScore) {
+                    return `You WIN! ${playerScore}-${computerScore}`;
+                }
+                else {
+                    return `You got beat ${computerScore}-${playerScore} by a Computer...`;
+                }
+            };
+            let endGame = document.createElement('div');
+            endGame.textContent = final();
+            document.body.appendChild(endGame);
+        }
+    };
+
+
+
 
