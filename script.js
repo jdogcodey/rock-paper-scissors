5324e7d12201a5ed6bae0d2299f3452e35d473c7
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-02-09 00:01:12 +0000

    Added some style... a little too much

--- script.js
+++ script.js
@@ -3,31 +3,51 @@ let playerScore = 0;
 let runningHigh = 0;
 let playerSelectionLC = '';
 let i = 0;
+document.body.style.backgroundColor = 'red';
 
-    console.log(`${runningHigh} game`)
-const rock = document.createElement('button');
+const rock = document.createElement('img');
 rock.textContent = 'Rock';
 document.body.appendChild(rock);
 rock.addEventListener('click', () => {
     playerSelectionLC = 'rock';
     playRound();
 });
+rock.src = "rock.png";
+rock.style.display = "block";
+rock.style.marginLeft = "auto";
+rock.style.marginRight = "auto";
+rock.style.width = "auto";
+rock.style.maxHeight = "170px";
 
-const paper = document.createElement('button');
+const paper = document.createElement('img');
 paper.textContent = 'Paper';
 document.body.appendChild(paper);
 paper.addEventListener('click', () => {
     playerSelectionLC = 'paper';
     playRound();
 });
+paper.src = "paper.png";
+paper.style.display = "block";
+paper.style.marginLeft = "auto";
+paper.style.marginRight = "auto";
+paper.style.width = "auto";
+paper.style.maxHeight = "170px";
 
-const scissors = document.createElement('button');
+
+const scissors = document.createElement('img');
 scissors.textContent = 'Scissors';
 document.body.appendChild(scissors);
 scissors.addEventListener('click', () => {
     playerSelectionLC = 'scissors';
     playRound();
 });
+scissors.src = "scissors.png";
+scissors.style.display = "block";
+scissors.style.marginLeft = "auto";
+scissors.style.marginRight = "auto";
+scissors.style.width = "auto";
+scissors.style.maxHeight = "170px";
+
 
 const scoreTracker = document.createElement('div');
 const results = document.createElement('div');
@@ -61,16 +81,19 @@ function playRound() {
             else return 'Something went wrong... Did you spell your choice correctly?'
         };
         results.textContent = outcome();
+        results.style.textAlign = "center";
+        results.style.fontSize = "20px";
+        results.style.fontWeight = "600";
         document.body.appendChild(results);
         function score() {
-            if (playerScore > computerScore) {
-            return `You're Winning! SCORE: Player-${playerScore} : ${computerScore}-Computer`;
-            }
-            else {
-                return `You're Losing SCORE: Player-${playerScore} : ${computerScore}-Computer`;
-            };
+            return `SCORE: Player-${playerScore} : ${computerScore}-Computer`;
+        
         }
         scoreTracker.textContent = score();
+        scoreTracker.style.textAlign = "center";
+        scoreTracker.style.fontSize = "40px";
+        scoreTracker.style.fontWeight = "800";
+        scoreTracker.style.color = "white";
         document.body.appendChild(scoreTracker);
         runningHigh = Math.max(computerScore, playerScore);
         if (runningHigh >= 5) {
@@ -89,6 +112,10 @@ function playRound() {
             };
             let endGame = document.createElement('div');
             endGame.textContent = final();
+            endGame.style.textAlign = "center";
+            endGame.style.fontSize = "80px";
+            endGame.style.fontWeight = "800";
+            endGame.style.color = "white";
             document.body.appendChild(endGame);
         }
     };
